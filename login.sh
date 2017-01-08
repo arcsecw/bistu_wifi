8bbafda4b672adda1575d20f0f0b56cedc2d3840
billhu1996 <billhu1996@gmail.com>
2017-01-08 18:18:04 +0800

    重新整理lgoin

--- login.sh
+++ login.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#login in
+#login
+
 username=你的用户名
 password=你的密码
-curl -d "buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network&username=`echo -n $username`&password=`echo -n $password`" "https://1.1.1.1/login.html"
+
+data="buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network&username="$username"&password="$password
+curl -d $data "https://1.1.1.1/login.html"
