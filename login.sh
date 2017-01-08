41ef537516fa5584b71ce0bc101d3b3d676113d8
billhu1996 <billhu1996@gmail.com>
2017-01-08 14:33:27 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,1 +1,5 @@
-curl -d "buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network&username=你的用户名&password=你的密码" "https://1.1.1.1/login.html"
+#!/bin/bash
+#login in
+username=你的用户名
+password=你的密码
+curl -d "buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network&username=`echo -n $username`&password=`echo -n $password`" "https://1.1.1.1/login.html"
